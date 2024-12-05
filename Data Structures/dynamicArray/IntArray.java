a1e01fe8514b19a1c99ab0c3e1aabcd2c4f601c9
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-12-05 18:07:26 +0600

    updated

--- Data Structures/dynamicArray/IntArray.java
+++ Data Structures/dynamicArray/IntArray.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 @SuppressWarnings("null")
+
 public class IntArray implements Iterable<Integer> {
+    private static final int DEFAULT_CAP = 1 << 3;
     public int[] arr;
     private int len = 0;
     private int capacity = 0;
 
     public IntArray() {
-        this(16);
+        this(DEFAULT_CAP);
     }
 
     public IntArray(int capacity) {
@@ -14,6 +18,14 @@ public class IntArray implements Iterable<Integer> {
         }
         this.capacity = capacity;
         arr = new int[capacity];
+    }
+
+    public IntArray(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("Array can not be null");
+        }
+        arr = java.util.Arrays.copyOf(array, array.length);
+        capacity = len = array.length;
     }
 
     public int size() {
@@ -53,6 +65,92 @@ public class IntArray implements Iterable<Integer> {
 
     }
 
+    // public void add(int elem) {
+    // if (len + 1 >= capacity) {
+    // if (capacity == 0) {
+    // capacity = 1;
+    // } else {
+    // capacity *= 2;
+    // }
+    // int[] new_arr = new int[capacity];
+    // for (int i = 0; i < len; i++) {
+    // new_arr[i] = arr[i];
+    // }
+    // arr = new_arr;
+
+    // }
+    // arr[len++] = elem;
+
+    // }
+
+    public void add(int elem) {
+        if (len + 1 > capacity) {
+            if (capacity == 0) {
+                capacity = 1;
+            } else {
+                capacity *= 2;
+            }
+            arr = java.util.Arrays.copyOf(arr, capacity);
+        }
+        arr[len++] = elem;
+
+    }
+
+    // public int removeAt(int index) {
+    // if (index >= len && index < 0) {
+    // throw new IndexOutOfBoundsException();
+    // } else {
+    // int data = arr[index];
+    // int[] new_arr = new int[len - 1];
+    // for (int i = 0, j = 0; i < len; i++, j++) {
+    // if (i == index)
+    // j--;
+    // else {
+    // new_arr[i] = arr[i];
+    // }
+    // }
+    // arr = new_arr;
+    // capacity = --len;
+    // return data;
+    // }
+    // }
+
+    public void removeAt(int rm_index) {
+        System.arraycopy(arr, rm_index + 1, arr, rm_index, len - rm_index - 1);
+        --len;
+        --capacity;
+    }
+
+    public boolean remove(int eleme) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == eleme) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void sort() {
+        java.util.Arrays.sort(arr, 0, len);
+    }
+
+    public void reverse() {
+        for (int i = 0; i < len / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[len - i - 1]; // 1st elem go to last according to slot
+            arr[len - i - 1] = temp; // last elem go to 1st according to slot
+        } // in this loop every time..2 values position will be changed..so it will work
+          // len/2 times;
+    }
+
+    public int binarySearch(int key) {
+        int index = java.util.Arrays.binarySearch(arr, 0, len, key);
+        // if (index < 0) index = -index - 1; // If not found this will tell you where
+        // to insert
+        return index;
+    }
+
     public java.util.Iterator<Integer> iterator() {
         return new java.util.Iterator<Integer>() {
             int index = 0;
@@ -64,6 +162,10 @@ public class IntArray implements Iterable<Integer> {
             public Integer next() {
                 return arr[index++];
             };
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
 
         };
     }
@@ -82,6 +184,20 @@ public class IntArray implements Iterable<Integer> {
 
     public static void main(String[] args) {
         IntArray ar = new IntArray(14);
+        ar.add(1);
+        ar.add(2);
+        ar.add(3);
+        ar.add(4);
+        ar.set(3, 5);
+        ar.reverse();
+        System.out.println(ar.binarySearch(3));
+        // ar.removeAt(1);
+        System.out.println(ar);
+
+        Iterator<Integer> it1 = ar.iterator();
+        // it1.remove();
+        System.out.println(it1.hasNext());
+
     }
 
 }
